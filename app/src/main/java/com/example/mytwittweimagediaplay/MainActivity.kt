21c9fd0b172c9b63d2187f68fe83cff273cd429a
Chukwuwa Uchenna <chukwuwauchenna@gmail.com>
2021-01-21 14:06:02 +0100

    added viewbinding and converted the layout to view binding

--- app/src/main/java/com/example/mytwittweimagediaplay/MainActivity.kt
+++ app/src/main/java/com/example/mytwittweimagediaplay/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mytwittweimagediaplay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytwittweimagediaplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding:ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
